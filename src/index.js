44036f82e4f2baa522a21c36877781be3367a390
João Oliveira <joaopedrooliveira6704@gmail.com>
2026-01-07 22:00:42 +0000

    feat: update environment configuration and add Vercel deployment settings

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const v1QuoteRouter = require("./routes/v1/quoteRoutes");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../docs/swagger.json");
+require('dotenv').config();
+const express = require('express');
+const v1QuoteRouter = require('./routes/v1/quoteRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Quotes API online");
+app.get('/', (req, res) => {
+  res.send('Quotes API online');
 });
 
-app.use("/api/v1/quotes", v1QuoteRouter);
+app.use('/api/v1/quotes', v1QuoteRouter);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(PORT, () => {
-  console.log(
-    `Quotes API listening on port ${PORT}.\nView docs: http:/localhost:${PORT}/api-docs`
-  );
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
+module.exports = app;
