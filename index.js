a777f9477cebbd651cd2336c3a54a524a10563ca
João Oliveira <joaopedrooliveira6704@gmail.com>
2026-01-07 22:31:10 +0000

    refactor: restructure index.js to improve organization and readability

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import app from './src/index.js';
+import 'dotenv/config';
+import express from 'express';
+import v1QuoteRouter from './src/routes/v1/quoteRoutes.js';
+/* import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Read swagger.json directly
+const swaggerDocument = JSON.parse(
+  readFileSync(join(__dirname, './docs/swagger.json'), 'utf-8')
+);
+ */
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Quotes API online');
+});
+
+app.use('/api/v1/quotes', v1QuoteRouter);
+/* app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); */
 
 export default app;
